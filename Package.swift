9016ef4e2deeab19ef136459b7e1cca01fc29857
tristan <tristan.augendre@teladoc.com>
2024-10-28 14:38:39 +0100

    Update release

--- Package.swift
+++ Package.swift
@@ -2,18 +2,18 @@
 import PackageDescription
 
 let package = Package(
-   name: "designSystem",
+   name: "SampleApp",
    platforms: [
       .iOS(.v14),
    ],
    products: [
-      .library(name: "designSystem", targets: ["designSystem"])
+      .library(name: "SampleApp", targets: ["SampleApp"])
    ],
    targets: [
       .binaryTarget(
-         name: "designSystem",
-         url: "https://github.com/TristanTHF/DesignSystemSPM/releases/download/0.0.1/designSystem.framework.zip",
-         checksum:"119f9b92d021ddbe15920385d390529bceb21bb5127ecda4842551e77aa1a9b5"
+         name: "SampleApp",
+         url: "https://github.com/TristanTHF/DesignSystemSPM/releases/download/0.0.1/SampleApp.xcframework.zip",
+         checksum:"f1c3d0e20c64cddafa011a56ff60aff4cb696a3fa911435b8a020f72e3c9b4f7"
       )
    ]
 )
