e7a7e220fc2901ac91a458449080bb931c008fb4
TristanTHF <121114162+TristanTHF@users.noreply.github.com>
2025-02-28 14:21:13 +0100

    Update Package.swift
    Upgrade version

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "designsystem",
-         url: "https://github.com/TristanTHF/DesignSystemSPM/releases/download/0.1.1/designsystem.xcframework.zip",
+         url: "https://github.com/TristanTHF/DesignSystemSPM/releases/download/0.1.2/designsystem.xcframework.zip",
          checksum:"cd0d01ba47ef55e9e3d7abb89654f564a050d1de2de9d0dd6c8e81cb91deda7c"
       )
    ]
